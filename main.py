feac278609ce2a14bc8965e44446aafef6283234
timurius <timur.yagudin.08@gmail.com>
2025-02-18 18:02:08 +0400

    change: made simple config functionality

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import asyncio
 from telethon import events, TelegramClient
+from json import load
 
-api_id = 0
-api_hash = ''
+with open('config.json', 'r') as config_file:
+    config = load(config_file)
+    api_id = config['api_id']
+    api_hash = config['api_hash']
+    if ( len(config['chats']) != 0 ):
+        chats_to_monitor = config['chats']
+    if ( len(config['trigger_words']) != 0 ):
+        trigger_words = config['trigger_words']
+    if ( config['notification_channel'] != '' ):
+        channel = config['notification_channel'] 
+    if ( config['reply_message'] != '' ):
+        reply_message = config['reply_message']  
+
 client = TelegramClient('anon', api_id, api_hash)
-reply_text = ''
-
-trigger_words = []
 
 @client.on(events.NewMessage())
 async def handler(event):
